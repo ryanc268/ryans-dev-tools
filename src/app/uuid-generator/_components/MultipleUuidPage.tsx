12f1265821022f0bf9ff82c58a4f44dc5c016bfa
Ryan Coppa <ryan.tcoppa@gmail.com>
2023-12-15 11:33:37 -0500

    Fixing Multiple Uuid generation flow

--- src/app/uuid-generator/_components/MultipleUuidPage.tsx
+++ src/app/uuid-generator/_components/MultipleUuidPage.tsx
@@ -5,6 +5,7 @@ import { OptionSelectStyle, UuidCopyOptions } from "@/global/enums";
 import { useEffect, useRef, useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { BsClipboard2Check, BsClipboard2 } from "react-icons/bs";
+import { MdOutlineRefresh } from "react-icons/md";
 
 const MultipleUuidPage = () => {
   const [copyOption, setCopyOption] = useState<UuidCopyOptions>(
@@ -12,6 +13,7 @@ const MultipleUuidPage = () => {
   );
   const [numToGenerate, setNumToGenerate] = useState<number | null>(1);
   const [uuidString, setUuidString] = useState<string>("");
+  const [uuidStringSql, setUuidStringSql] = useState<string>("");
   const [isCopied, setIsCopied] = useState(false);
 
   const buttonRef = useRef<HTMLButtonElement>(null);
@@ -33,20 +35,25 @@ const MultipleUuidPage = () => {
   const generateUuids = () => {
     if (!numToGenerate || numToGenerate < 1) return;
     let tempString = "";
-    if (copyOption === UuidCopyOptions.REGULAR) {
-      for (let i = 0; i < numToGenerate; i++) {
-        tempString += crypto.randomUUID() + "\n";
-      }
-    } else if (copyOption === UuidCopyOptions.SQL) {
-      tempString += "(";
-      for (let i = 0; i < numToGenerate; i++) {
-        tempString += `\"${crypto.randomUUID()}\"${
-          i !== numToGenerate - 1 ? ", \n" : ""
-        }`;
-      }
-      tempString += ")";
+    for (let i = 0; i < numToGenerate; i++) {
+      tempString += crypto.randomUUID() + "\n";
     }
+
+    let tempStringSql = "(";
+    for (let i = 0; i < numToGenerate; i++) {
+      tempStringSql += `\"${crypto.randomUUID()}\"${
+        i !== numToGenerate - 1 ? ", \n" : ""
+      }`;
+    }
+    tempStringSql += ")";
+
     setUuidString(tempString);
+    setUuidStringSql(tempStringSql);
+  };
+
+  const displayUuidString = () => {
+    if (copyOption === UuidCopyOptions.SQL) return uuidStringSql;
+    return uuidString;
   };
 
   const toggleCopy = () => {
@@ -64,15 +71,25 @@ const MultipleUuidPage = () => {
     <div className="flex w-full flex-col items-center">
       <div className="mb-4 flex w-1/2 flex-col items-center">
         <h4 className="my-1 text-sm md:text-base 2xl:text-lg">
-          Select the amount to generate
+          Enter the amount to generate
         </h4>
-        <input
-          type="text"
-          maxLength={3}
-          className="w-1/2 rounded-lg border bg-transparent p-2 text-center md:w-1/4"
-          value={numToGenerate || ""}
-          onChange={(e) => validateNumber(e.target.value)}
-        />
+        <div className="flex items-center justify-center">
+          <input
+            type="text"
+            maxLength={3}
+            className="w-1/2 rounded-lg border bg-transparent p-2 text-center md:w-1/4"
+            value={numToGenerate || ""}
+            onChange={(e) => validateNumber(e.target.value)}
+          />
+          <button
+            className="ml-4 flex items-center rounded-lg border p-2 text-lg hover:bg-zinc-700 disabled:opacity-25 disabled:hover:bg-transparent md:text-xl"
+            disabled={numToGenerate && numToGenerate > 0 ? false : true}
+            onClick={() => generateUuids()}
+            ref={buttonRef}
+          >
+            <MdOutlineRefresh />
+          </button>
+        </div>
       </div>
       <div className="flex w-2/3 flex-row justify-center md:w-1/3">
         <OptionSelect
@@ -81,19 +98,11 @@ const MultipleUuidPage = () => {
           style={OptionSelectStyle.GRAY}
         />
       </div>
-      <button
-        className="mt-4 rounded-lg border px-2 py-1 text-sm hover:bg-zinc-800 disabled:opacity-25 disabled:hover:bg-transparent md:px-4 md:py-2 md:text-base"
-        disabled={numToGenerate && numToGenerate > 0 ? false : true}
-        onClick={() => generateUuids()}
-        ref={buttonRef}
-      >
-        Generate
-      </button>
       <textarea
         className="my-4 w-5/6 resize-none rounded-lg border bg-transparent p-2 text-sm md:mx-0 md:w-1/2 md:text-xl 2xl:w-1/4"
         readOnly
         rows={10}
-        value={uuidString}
+        value={displayUuidString()}
       />
       <CopyToClipboard text={uuidString} onCopy={() => toggleCopy()}>
         <button
